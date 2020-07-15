89b7d08957a2e838fd76e2143a2bf3299419697b
libitx <libitx@protonmail.com>
2020-07-15 22:59:48 +0100

    Move build to docs for ghpages

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = (env, options) => {
     },
 
     output: {
-      path: path.resolve(__dirname, 'dist'),
+      path: path.resolve(__dirname, 'docs'),
       filename: '[name].js',
       libraryTarget: 'umd'
     },
