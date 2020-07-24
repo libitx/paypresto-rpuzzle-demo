2161cc98c5be384d5a6e9b549ddb67ffa1f3e8a3
libitx <libitx@protonmail.com>
2020-07-24 23:50:18 +0100

    Use latest paypresto lib

--- src/components/SlideOver.vue
+++ src/components/SlideOver.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="fixed top-0 left-0 right-0 bottom-0 bg-black-60"
     @click.self="$emit('close')">
-    <div class="absolute top-0 bottom-0 right-0 bg-mid-gray shadow-2 | slide-over">
+    <div class="absolute top-0 bottom-0 right-0 bg-white shadow-2 | slide-over">
       <div class="f6 tr bg-white">
         <a class="dib pa2 link hot-pink dim pointer" @click="$emit('close')">Close</a>
         <div ref="widget"></div>
